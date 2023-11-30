109e48c51e46800297d03eb10817c49c320d5477
mmirshod <mirshodmakhsudov@gmail.com>
2023-11-30 18:48:42 +0500

    SignIn Finished!

---
+++ src/main/java/com/sunnydevs/alengame/SignInController.java
@@ -0,0 +1,66 @@
+package com.sunnydevs.alengame;
+
+import com.sunnydevs.alengame.db.User;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class SignInController {
+    @FXML
+    TextField username;
+    @FXML
+    PasswordField password;
+    @FXML
+    Button signInBtn, signUpBtn;
+
+    @FXML
+    void initialize() {
+        signInBtn.setOnAction(signInHandler);
+        signUpBtn.setOnAction(signUpHandler);
+    }
+
+    EventHandler<ActionEvent> signUpHandler = e -> {
+        try {
+            ((Button) e.getSource()).getScene().setRoot((new FXMLLoader(getClass().getResource("SignUp.fxml")).load()));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    };
+
+    EventHandler<ActionEvent> signInHandler = event -> {
+        String pwd = password.getText();
+        String user = username.getText();
+        try {
+            System.out.println(User.getUser(user).password());
+            System.out.println(PasswordManager.hashPassword(pwd));
+            System.out.println(PasswordManager.checkPassword(pwd, User.getUser(user).password()));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            if (!PasswordManager.checkPassword(pwd, User.getUser(user).password())) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Login Failed");
+                alert.setHeaderText("Incorrect Password");
+                alert.setContentText("The entered password is incorrect. Please try again.");
+
+                alert.showAndWait();
+            } else {
+                Stage thisStage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("home.fxml"));
+                thisStage.getScene().setRoot(fxmlLoader.load());
+            }
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    };
+}

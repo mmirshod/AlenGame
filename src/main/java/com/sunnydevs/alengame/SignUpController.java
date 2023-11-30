ec85e95d8b68305954807eb1348a001c0100b703
mmirshod <mirshodmakhsudov@gmail.com>
2023-11-30 18:49:02 +0500

    SignUp Finished!

---
+++ src/main/java/com/sunnydevs/alengame/SignUpController.java
@@ -0,0 +1,61 @@
+package com.sunnydevs.alengame;
+
+import com.sunnydevs.alengame.db.User;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.StringBinding;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public final class SignUpController {
+    @FXML
+    TextField username, age, firstName, lastName;
+    @FXML
+    PasswordField password;
+    @FXML
+    Button signInBtn, signUpBtn;
+
+    @FXML
+    private void initialize() {
+        bindUsernameProperty();
+        signUpBtn.setOnAction(signUpHandler);
+        signInBtn.setOnAction(signInHandler);
+    }
+
+    private final EventHandler<ActionEvent> signUpHandler = e -> {
+        String firstNameInput = firstName.getText();
+        String lastNameInput = lastName.getText();
+        int ageInput = Integer.parseInt(age.getText());
+        String password = PasswordManager.hashPassword(this.password.getText());
+
+        User.signUp(firstNameInput, lastNameInput, ageInput, password);
+    };
+
+    private final EventHandler<ActionEvent> signInHandler = e -> {
+        try {
+            ((Button) e.getSource()).getScene().setRoot((new FXMLLoader(getClass().getResource("SignIn.fxml")).load()));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    };
+
+    private void bindUsernameProperty() {
+        StringBinding concatenatedValue = Bindings.createStringBinding(
+                () -> String.format("%s%s%s",
+                        firstName.getText().toLowerCase(),
+                        lastName.getText().toLowerCase(),
+                        age.getText()
+                ),
+                firstName.textProperty(), lastName.textProperty(), age.textProperty()
+        );
+
+        // Bind the textProperty of username to the concatenated value
+        username.textProperty().bind(concatenatedValue);
+    }
+}
